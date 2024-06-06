fb348999000f80b608f54a7cbcb7b340063e89b0
Sanket <sanketgupta1000@gmail.com>
2024-06-06 14:44:19 +0530

    basic jwt config

--- src/main/java/com/project/readers/readers_community/configs/SecurityConfig.java
+++ src/main/java/com/project/readers/readers_community/configs/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.project.readers.readers_community.configs;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,19 +13,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig
 {
-
+    private final RsaKeyProperties rsaKeys;
     private final UserDetailsService customUserDetailsService;
 
     // DI
-    public SecurityConfig(UserDetailsService customUserDetailsService)
+    public SecurityConfig(UserDetailsService customUserDetailsService, RsaKeyProperties rsaKeys)
     {
         this.customUserDetailsService = customUserDetailsService;
+        this.rsaKeys = rsaKeys;
     }
 
     // security config bean
@@ -33,11 +46,40 @@ public class SecurityConfig
                         auth
                             .anyRequest().authenticated();
                 })
+                // for jwt
+                .oauth2ResourceServer(
+                        oauth2ResourceServer->
+                                oauth2ResourceServer.jwt(
+                                        jwt->jwt.decoder(jwtDecoder())
+                                )
+                )
                 // custom user details service
                 .userDetailsService(customUserDetailsService)
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    // bean for decoding jwts
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    // for encoding
+    @Bean
+    public JwtEncoder jwtEncoder()
+    {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    // bean for password encoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
