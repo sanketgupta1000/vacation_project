5632855fae7fe90a19b7f1645336d93b40cf509a
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2024-06-11 14:49:47 +0530

    Added endpoints and service methods for book-transaction

--- src/main/java/com/project/readers/readers_community/controllers/BookController.java
+++ src/main/java/com/project/readers/readers_community/controllers/BookController.java
@@ -1,23 +1,20 @@
 package com.project.readers.readers_community.controllers;
 
-import com.project.readers.readers_community.entities.BookCategory;
+import com.project.readers.readers_community.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.project.readers.readers_community.annotations.CurrentUser;
-import com.project.readers.readers_community.entities.Book;
-import com.project.readers.readers_community.entities.User;
 import com.project.readers.readers_community.services.BookService;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController
 {
-	
 	private final BookService bookService;
 
 	public BookController(BookService bookService)
@@ -25,6 +22,22 @@ public class BookController
 		this.bookService = bookService;
 	}
 
+	@PostMapping("/{book_copy_id}/initiate_handover")
+	public String initiate_handover(@PathVariable("book_copy_id")BookCopy bookCopy, @CurrentUser User currentUser)
+	{
+		return bookService.initiate_handover(bookCopy, currentUser);
+	}
 
+	@PostMapping("/{book_copy_id}/handover")
+	public String handoverBookCopy(@PathVariable("book_copy_id")BookCopy bookCopy, @CurrentUser User currentUser, @RequestParam("otp") String otp)
+	{
+		return bookService.handoverBookCopy(bookCopy, currentUser, otp);
+	}
+
+	@GetMapping("/{book_copy_id}/transaction_history")
+	public List<BookTransaction> getBookTransactions(@PathVariable("book_copy_id")BookCopy bookCopy, @CurrentUser User currentUser)
+	{
+		return bookService.getBookTransactions(bookCopy, currentUser);
+	}
 
 }
--- src/main/java/com/project/readers/readers_community/repositories/UserRepository.java
+++ src/main/java/com/project/readers/readers_community/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.project.readers.readers_community.repositories;
 
 import com.project.readers.readers_community.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Integer>
 {
