5153d19be8b7b3ad49ec0403e2105bec26bb566e
Sanket <sanketgupta1000@gmail.com>
2024-06-14 09:29:29 +0530

    refactored get all references

--- src/main/java/com/project/readers/readers_community/repositories/UserRepository.java
+++ src/main/java/com/project/readers/readers_community/repositories/UserRepository.java
@@ -8,9 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer>
 {
     User findByEmail(String email);
-
-	List<User> findByReferrer(User user);
-    
-    
-    
 }
